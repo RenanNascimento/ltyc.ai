5d2dc73cf2a1133bc089596edc0499cbf5bac801
renan.nascimento <renan.nascimento@quintoandar.com.br>
2024-12-15 12:18:12 -0300

    add simple agent

--- agent.py
+++ agent.py
@@ -4,19 +4,36 @@ from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-from langchain_ollama import ChatOllama
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import PromptTemplate
 
-llm = ChatOllama(model="llama3", request_timeout=360.0)
-prompt = ChatPromptTemplate.from_template("You`re a helpful agent, just talk with the user about what he wants: {topic}")
+from youtube_transcript_api import YouTubeTranscriptApi
+
+from langchain_openai import ChatOpenAI
+
+def get_transcript(url: str) -> str:
+    """Get yourube video transcript"""
+
+    if url.find("youtube"):
+        url = url.replace("https://www.youtube.com/watch?v=", "")
+
+    transcript = YouTubeTranscriptApi.get_transcript(url)
+    return transcript
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+prompt = PromptTemplate(
+    template="""Show the transcript bellow 
+            '{transcript}'""",
+    input_variables=["transcript"],
+)
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 def chatbot(state: State):
-    chatbot_runnable = prompt | llm
+    transcript = get_transcript(state["messages"][-1].content)
 
-    return {"messages": [chatbot_runnable.invoke(state["messages"])]}
+    chatbot_runnable = prompt | llm
+    return {"messages": [chatbot_runnable.invoke(transcript)]}
 
 graph_builder = StateGraph(State)
 
